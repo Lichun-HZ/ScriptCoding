7ffcecd4d18b2da296aca158dff67522954e7965
Lichun <lcbiotech@126.com>
2024-07-26 00:10:29 +0800

    First Commit

---
+++ Python/Tutorial/BasicDataType.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+# Number, String, Tuple, List, Set, Dictionary
+
+# 1. Number: int, float, bool, complex
+a, b, c, d = 20, 5.5, True, 4+3j
+print(type(a), type(b), type(c), type(d))
